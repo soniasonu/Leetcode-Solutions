a13f3f571756129c2f52cb9ed9bea6e13c1c1495
Sonia J <soniajayesh760@gmail.com>
2026-08-03 13:56:41 +0530

    Implement palindrome check in valid-palindrome.py

---
+++ strings/valid-palindrome.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        num = x
+        result = 0 
+        while x > 0:
+            ld = x % 10
+            result = (result * 10) + ld
+            x = x // 10
+        return num == result
+
+
+        
