212994b63dfbffbbe77a04c52bbbfc8d4238b377
Sonia J <soniajayesh760@gmail.com>
2026-08-03 13:12:43 +0530

    Add Two Sum Solution
    Add Two Sum problem solution in two-sum.py . Implement Two Sum solution with O(n) time complexity.

---
+++ arrays/two-sum.py
@@ -0,0 +1,13 @@
+# 1. Two Sum
+# https://leetcode.com/problems/two-sum/
+# Time: O(n), Space: O(n)
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+                
+        
