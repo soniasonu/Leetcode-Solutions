b0f62bff8e014b2415ae49a632248a772ea46bb0
Sonia J <soniajayesh760@gmail.com>
2026-08-15 19:20:05 +0530

    Create Third-maximum-number.py

---
+++ arrays/Third-maximum-number.py
@@ -0,0 +1,17 @@
+#414
+class Solution(object):
+    def thirdMax(self, nums):
+        first = second = third = None
+        for num in nums:
+            if num == first or num == second or num == third:
+                continue
+            if first is None or num > first:
+                third = second
+                second = first
+                first = num
+            elif second is None or num > second:
+                third = second
+                second = num
+            elif third is None or num > third:
+                third = num
+        return first if third is None else third
