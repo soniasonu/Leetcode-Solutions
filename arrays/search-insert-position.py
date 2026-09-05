8a68052422098aac60c6f65a165547b30ee37174
Sonia J <soniajayesh760@gmail.com>
2026-09-05 20:16:58 +0530

    Create search-insert-position.py

---
+++ arrays/search-insert-position.py
@@ -0,0 +1,15 @@
+#35
+class Solution(object):
+    def searchInsert(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+        mis = 0
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return mid + 1 if target > nums[mid] else mid
