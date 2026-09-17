2a2cce2b56d9474c8ac7ea7296eec4a08a5c7870
Sonia J <soniajayesh760@gmail.com>
2026-09-17 22:10:03 +0530

    Create Rotate-array.py

---
+++ arrays/Rotate-array.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        n = len(nums)
+        k %= n
+        for _ in range(k):
+            last = nums.pop()
+            nums.insert(0, last)
